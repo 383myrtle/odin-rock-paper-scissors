e4a691a841495df297f5081df64f771c233f190e
383myrtle <89387525+383myrtle@users.noreply.github.com>
2023-09-14 11:30:53 +0200

    Add playRound function

--- javascript.js
+++ javascript.js
@@ -8,3 +8,22 @@ function getComputerChoice(){
     return options[getRandomInt(3)]
 }
 
+function playRound(playerSelection, computerSelection){
+    let pl = playerSelection.slice(0,1).toUpperCase().concat("",playerSelection.slice(1)) //convert player selection to title case
+    let com = computerSelection //computer selection already in title case
+
+    if (options.indexOf(pl)==-1){
+        return "Please enter a valid option"
+    }
+    if (pl==com){
+        return `It's a draw! You both threw ${com}`
+    }
+
+    let plIndex = options.indexOf(pl)
+    if (options.indexOf(com)==(plIndex+1)%3){
+        return `You lose :( you threw ${pl} while the computer threw ${com}`
+    }
+    else{
+        return `You win! You threw ${pl} while the computer threw ${com}`
+    }
+}
